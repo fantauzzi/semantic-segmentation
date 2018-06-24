fdd8a8f26f6c5c906ad31cc282fcfc4c5d14d735
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2018-06-24 19:49:40 +0200

    Improved dataset loading and shuffling and added images normalization

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import numpy as np
 
 # session = get_session()
 config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
+config.gpu_options.allow_growth = False
 session = tf.Session(config=config)
 set_session(session)
 
@@ -48,11 +48,6 @@ for idx, layer in enumerate(reversed(encoder.layers)):
         x = UpSampling2D(size=layer.pool_size,
                          name=layer.name + '_dec')(x)
     elif type(layer) == Conv2D:  # Convolution -> same convolution, but with no activation function
-        '''
-        Set the activation function to None, unless the layer is the last in the decoder (corresponding to the first in the encoder), then set it to softmax
-        '''
-        # activation = 'softmax' if idx == n_encoder_layers-2 else None
-
         x = Conv2D(filters=layer.filters,
                    kernel_size=layer.kernel_size,
                    activation=None,
@@ -79,12 +74,6 @@ def get_gt_file_name(file_name):
     return road_name, lane_name
 
 
-# Load the first image in the dataset to discover the images resolution
-
-# first_image_path = next(training_path.glob('*.png'))
-# first_image = imread(first_image_path)
-# image_res_y, image_res_x, image_res_depth = first_image.shape
-
 def load_dataset(images_path, gt_images_path):
     n_images = sum(1 for _ in images_path.glob('*.png'))
 
@@ -93,11 +82,13 @@ def load_dataset(images_path, gt_images_path):
     '''
     X = np.zeros(shape=(n_images, input_shape[0], input_shape[1], input_shape[2]))
     Y = np.zeros(shape=(n_images, input_shape[0], input_shape[1], n_classes))
+    paths = np.empty(n_images, dtype=np.str)
 
     # For every image in the dataset...
     for idx, image_path in enumerate(images_path.glob('*.png')):
         # ... load the image and add it to X
-        image = imresize(imread(image_path), (input_shape[0], input_shape[1]))  # TODO try with different interpolations, also for the GT
+        image = imresize(imread(image_path),
+                         (input_shape[0], input_shape[1]))  # TODO try with different interpolations, also for the GT
         X[idx, :, :, :] = image
         # Find the file name of the image with the corresponding ground truth
         gt_image_name, _ = get_gt_file_name(image_path.resolve().name)
@@ -106,50 +97,70 @@ def load_dataset(images_path, gt_images_path):
         # Load the ground truth image and add it to Y (1-hot encoded)
         gt_image = imresize(imread(gt_image_path), (input_shape[0], input_shape[1]))
         gt_image = gt_image[:, :, 2]
-        gt_image[gt_image>=128] = 1  # TODO is this a good idea?
+        gt_image[gt_image >= 128] = 1  # TODO is this a good idea?
         gt_image[gt_image < 128] = 0
-        Y[idx, : , :, 1] = gt_image
-        Y[idx, :, :, 0] = 1 - Y[idx, : , :, 1]
+        Y[idx, :, :, 1] = gt_image
+        Y[idx, :, :, 0] = 1 - Y[idx, :, :, 1]
+        paths[idx] = image_path
 
-    return X, Y
+    return X, Y, paths
 
-X, Y = load_dataset(training_path, gt_path)
+
+def split_dataset_with_paths(X, Y, paths, train_size, shuffle=True):
+    assert len(X) == len(Y) == len(paths)
+    assert 0 <= train_size <= 1
+    permutations = np.random.permutation(len(paths)) if shuffle else range(len(paths))
+    n_train = round(len(paths) * train_size)
+    X_shuffled, Y_shuffled, paths_shuffled = X[permutations], Y[permutations], paths[permutations]
+    X_train = X_shuffled[: n_train]
+    Y_train = Y_shuffled[: n_train]
+    paths_train = paths_shuffled[: n_train]
+    X_test = X_shuffled[n_train:]
+    Y_test = Y_shuffled[n_train:]
+    paths_test = paths_shuffled[n_train:]
+    return {'X_train': X_train,
+            'X_test': X_test,
+            'Y_train': Y_train,
+            'Y_test': Y_test,
+            'paths_train': paths_train,
+            'paths_test': paths_test}
+
+
+X, Y, image_paths = load_dataset(training_path, gt_path)
+X = X/255
 print('Loaded {} training images'.format(X.shape[0]))
 
+split = split_dataset_with_paths(X=X, Y=Y, paths=image_paths, train_size=.8)
+
+X_train = split['X_train']
+Y_train = split['Y_train']
+X_val = split['X_test']
+Y_val = split['Y_test']
+paths_train = split['paths_train']
+paths_val = split['paths_test']
 
 # TODO add data augmentation
 # TODO pre-process and normalize input images (what color space is best?)
 
-# X_train, X_val, Y_train, Y_val = train_test_split(X, Y, shuffle=True, train_size=.8, test_size=.2)
-
 optimizer = Adam()
 model.compile(optimizer=optimizer, loss='categorical_crossentropy')
-model.fit(x=X, y=Y, batch_size=2, epochs=1, verbose=1, validation_split=.2, shuffle=True)
+model.fit(x=X_train,
+          y=Y_train,
+          validation_data=(X_val, Y_val),
+          batch_size=2,
+          epochs=1,
+          verbose=1,
+          shuffle=True)
 
-X_test, Y_test = load_dataset(training_path, gt_path)
+X_test, Y_test, _ = load_dataset(training_path, gt_path)
 print('Loaded {} test images'.format(X_test.shape[0]))
 
 Y_pred = model.predict(x=X_test, batch_size=2, verbose=0)
 
 for idx, (x_test, y_pred) in enumerate(zip(X_test, Y_pred)):
-    image_mask = (y_pred[:, :, 1] > .5)*1
-    x_test[:, :, 0] = x_test[:, :, 0] + image_mask * 64
-    x_test = np.minimum(x_test, np.ones_like(x_test)*255)
-    # image_mask = np.stack((np.zeros((224,224)), image_mask*255, np.zeros((224,224)), np.ones((224, 224))*128), -1)
-    # x_test = np.append(x_test, np.ones((224,224,1))*255, -1)
-    # image = toimage(x_test, mode='RGBA')
-    # image.paste(image_mask, box=None, mask=image_mask)
-    imsave('output/'+str(idx)+'.png', x_test)
-
-
-
-"""im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
-segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
-mask = np.dot(segmentation, np.array([[0, 255, 0, 127]]))
-mask = scipy.misc.toimage(mask, mode="RGBA")
-street_im = scipy.misc.toimage(image)
-street_im.paste(mask, box=None, mask=mask)
-
-yield os.path.basename(image_file), np.array(street_im)"""
+    image_mask = (y_pred[:, :, 1] > .5) * 1
+    x_test[:, :, 0] = x_test[:, :, 0] + image_mask * 255
+    x_test = np.minimum(x_test, np.ones_like(x_test) * 255)
+    imsave('output/' + str(idx) + '.png', x_test)
 
 session.close()  # Closing the session prevents a non-systematic error at the end of program execution
